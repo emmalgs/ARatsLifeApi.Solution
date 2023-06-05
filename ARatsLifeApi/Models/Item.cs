5e051cf3cc9f52ec82a84bee0418682e4898cc6e
Kyle Crawford <kcraw217@gmail.com>
2023-06-05 15:45:19 -0700

    add Value

--- ARatsLifeApi/Models/Item.cs
+++ ARatsLifeApi/Models/Item.cs
@@ -6,5 +6,6 @@ public class Item
   public int ItemId { get; set; }
   [Required(ErrorMessage = "The item needs a name!")]
   public string Name { get; set; }
+  public int Value { get; set; }
   public int InventoryId { get; set; }
 }
