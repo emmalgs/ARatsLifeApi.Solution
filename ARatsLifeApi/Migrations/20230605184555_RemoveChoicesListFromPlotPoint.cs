f883f320d4420380732df7c0a0c40b898bdfdcd9
Kyle Crawford <kcraw217@gmail.com>
2023-06-05 11:53:31 -0700

    remove Choices list from plotpoint

---
+++ ARatsLifeApi/Migrations/20230605184555_RemoveChoicesListFromPlotPoint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARatsLifeApi.Migrations
+{
+    public partial class RemoveChoicesListFromPlotPoint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
