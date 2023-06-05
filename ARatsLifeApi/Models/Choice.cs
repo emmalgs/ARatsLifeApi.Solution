fd655a2873c5701bb640de2649b6dbd849ae8b21
Kyle Crawford <kcraw217@gmail.com>
2023-06-05 14:25:30 -0700

    remove ref nav prop

--- ARatsLifeApi/Models/Choice.cs
+++ ARatsLifeApi/Models/Choice.cs
@@ -7,5 +7,4 @@ public class Choice
   [Required(ErrorMessage = "The choice needs a decription!")]
   public string Description { get; set; }
   public int PlotpointId { get; set; }
-  public Plotpoint Plotpoint { get; set; }
 }
