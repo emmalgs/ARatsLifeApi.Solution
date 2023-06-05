603fe065011a2b68f94c863ec169bddee82209cf
Kyle Crawford <kcraw217@gmail.com>
2023-06-05 15:45:12 -0700

    add HeatLevel

--- ARatsLifeApi/Models/Choice.cs
+++ ARatsLifeApi/Models/Choice.cs
@@ -6,5 +6,6 @@ public class Choice
   public int ChoiceId { get; set; }
   [Required(ErrorMessage = "The choice needs a decription!")]
   public string Description { get; set; }
+  public int HeatLevel { get; set; } 
   public int PlotpointId { get; set; }
 }
