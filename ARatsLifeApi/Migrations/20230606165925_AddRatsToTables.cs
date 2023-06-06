71703014ec880351947944c1b26826bfc365f77f
Kyle Crawford <kcraw217@gmail.com>
2023-06-06 14:04:37 -0700

    Add Rats entity to database
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>
    dquote> co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>"

---
+++ ARatsLifeApi/Migrations/20230606165925_AddRatsToTables.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARatsLifeApi.Migrations
+{
+    public partial class AddRatsToTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rats",
+                columns: table => new
+                {
+                    RatId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Heat = table.Column<int>(type: "int", nullable: false),
+                    InventoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rats", x => x.RatId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Rats",
+                columns: new[] { "RatId", "Heat", "InventoryId", "Name" },
+                values: new object[] { 1, 40, 1, "Remy le Rouge" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rats");
+        }
+    }
+}
