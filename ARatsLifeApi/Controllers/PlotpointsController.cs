d08076d6df1dcce3d482251973967a6e2c1cdd1d
Kyle Crawford <kcraw217@gmail.com>
2023-06-06 15:40:01 -0700

    include choices list in get
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>
    dquote> co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>"

--- ARatsLifeApi/Controllers/PlotpointsController.cs
+++ ARatsLifeApi/Controllers/PlotpointsController.cs
@@ -12,7 +12,7 @@ namespace ARatsLifeApi.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-[Authorize]
+// [Authorize]
 public class PlotpointsController : ControllerBase
 {
   private readonly ARatsLifeApiContext _context;
@@ -26,25 +26,25 @@ public class PlotpointsController : ControllerBase
   [HttpGet]
   public async Task<ActionResult<IEnumerable<Plotpoint>>> GetPlotpoints()
   {
-    return await _context.Plotpoints.ToListAsync();
+    return await _context.Plotpoints
+                                  .Include(p => p.Choices)
+                                  .ToListAsync();
   }
 
   // GET: api/Plotpoints/5
   [HttpGet("{id}")]
-  public async Task<ActionResult<Dictionary<string, object>>> GetPlotpoint(int id)
+  public async Task<ActionResult<Plotpoint>> GetPlotpoint(int id)
   {
-    var scene = new Dictionary<string, object> () {};
-    var plotpoint = await _context.Plotpoints.FindAsync(id);
-    var choices = await _context.Choices.Where(c => c.PlotpointId == id).ToListAsync();
-    scene.Add("plotpoint", plotpoint);
-    scene.Add("choices", choices);
+    var plotpoint = await _context.Plotpoints
+                                            .Include(p => p.Choices)
+                                            .FirstOrDefaultAsync(p => p.PlotpointId == id);
 
     if (plotpoint == null)
     {
       return NotFound();
     }
 
-    return scene;
+    return plotpoint;
   }
 
   // PUT: api/Plotpoints/5
